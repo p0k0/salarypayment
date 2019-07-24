255e213abd873842213429148d2df5a871cafb94
p0k0 <m11e@ya.ru>
2019-07-24 20:04:11 +0300

    dto added

--- salary.dal/IRepository.cs
+++ salary.dal/IRepository.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace salary.dal
 {
     public interface IRepository
     {
-        
+        salary.dto.Employee Get(string name);
+        void Save(salary.dto.Employee employee);
+
+        IEnumerable<salary.dto.Employee> GetMany(int skip, int limit);
+
+        salary.dto.Employee GetMostExpensiveEmployee(short kind);
+
+        double GetMonthlyCost();
     }
 }
---
+++ salary.dto/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace salary.dto
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public double Rate { get; set; }
+        public short Kind { get; set; }
+    }
+}
