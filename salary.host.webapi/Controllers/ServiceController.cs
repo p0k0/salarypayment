37b17c090c55862056651bf34aec9dff6100127a
p0k0 <m11e@ya.ru>
2019-07-29 16:58:01 +0300

    ch1

---
+++ salary.host.webapi/Controllers/ServiceController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using salary.domain;
+using salary.service;
+
+namespace salary.host.webapi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+        // GET api/v1/service
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/v1/service/employee/name
+        [HttpGet("employee/{name}")]
+        public ActionResult<EmployeeBase> GetEmployee(string name, [FromServices]ISalaryService service)
+        {
+            return Ok(service.Get(name));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
--- salary.host.webapi/Startup.cs
+++ salary.host.webapi/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using salary.dal;
+using salary.dal.repository;
+using salary.service;
 
 namespace salary.host.webapi
 {
@@ -38,6 +41,8 @@ namespace salary.host.webapi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddTransient<IRepository, MySqlRepository>(c => new MySqlRepository(Configuration["DataBaseConnection:MySql"]));
+            services.AddSingleton<ISalaryService, SalaryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
