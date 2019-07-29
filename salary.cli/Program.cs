0b0b2db3dd6a340412f6bb105a24ee0073b78fa0
p0k0 <m11e@ya.ru>
2019-07-29 14:19:53 +0300

    cli, host.webapi empty proj added

---
+++ salary.cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace salary.cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
