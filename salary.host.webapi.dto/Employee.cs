9bed4bc82f98e11bb798ea890a6c577a7da3b21e
p0k0 <m11e@ya.ru>
2019-08-01 12:01:57 +0300

    webapi datamodel annotaion added

--- salary.host.webapi.dto/Employee.cs
+++ salary.host.webapi.dto/Employee.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace salary.host.webapi.dto
 {
     public class Employee
     {
         public string Id { get; set; }
+        
+        //[MaxLength(20, ErrorMessage = "Max length is 20 symbols")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,20}$", ErrorMessage = "Maximum 20-character are allowed.")]
         public string Name { get; set; }
+        
+        [RegularExpression(@"^[0-9]{1,20}$", ErrorMessage = "Maximum 20-digit number allowed.")]
         public string Rate { get; set; }
+        
+        [RegularExpression(@"^(monthly|hourly){1}$", ErrorMessage = "Only 'monthly' or 'hourly' are allowed.")]
         public string Kind { get; set; }
     }
 }
